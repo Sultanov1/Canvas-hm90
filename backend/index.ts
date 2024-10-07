64dca770656b5152fd3950ff082cc31ed0395591
Oybek Sultanov <oibekzzz09@gmail.com>
2024-10-07 15:34:09 +0600

    Init App

---
+++ backend/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import expressWs from  "express-ws";
+import cors from "cors";
+
+const app = express();
+expressWs(app);
+app.use(cors());
+
+const port = 8000;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port} port`);
+})
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            App init
+        </div>
+    );
+};
+
+export default App;
